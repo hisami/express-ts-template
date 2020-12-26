6b84f8a4be94a93d1f9d454e81d641c400cd7b1e
hisami <koron.take@gmail.com>
2020-12-26 15:18:05 +0900

    feat: eslint,prettierの導入

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
-import express from 'express'
-import userRouter from './routes/user';
+import express from "express";
+import userRouter from "./routes/user";
 
-const app: express.Express = express()
+const app: express.Express = express();
 
 // CORSの許可
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // リクエストの解析
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ルーティング
-app.use('/api/users', userRouter)
+app.use("/api/users", userRouter);
 
 // 3000番ポートでサーバ起動
-app.listen(3000, () => { console.log('listening on port 3000!') })
+app.listen(3000, () => {
+  console.log("listening on port 3000!");
+});
