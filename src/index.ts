e125c5393a078e57c751944fb13f33b5124aa0e8
hisami <koron.take@gmail.com>
2020-12-26 12:38:42 +0900

    feat: GET/POSTリクエスト

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import userRouter from './routes/user';
+
+const app: express.Express = express()
+
+// CORSの許可
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+// リクエストの解析
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// ルーティング
+app.use('/api/users', userRouter)
+
+// 3000番ポートでサーバ起動
+app.listen(3000, () => { console.log('listening on port 3000!') })
---
+++ src/routes/user.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const router: express.Router = express.Router();
+
+/* GET */
+router.get('/', (req: express.Request, res: express.Response) => {
+  res.send('get api is running');
+});
+
+/* POST */
+router.post('/', (req: express.Request, res: express.Response) => {
+  console.log(req.body)
+  res.send('post api is running');
+});
+
+export default router;
