302449a1c1f85bf4a7844e1da67f9e8a7786fb76
hisami <koron.take@gmail.com>
2020-12-29 13:05:13 +0900

    refactor: controllerテスト

--- src/tests/unit/controllers/userController.test.ts
+++ src/tests/unit/controllers/userController.test.ts
@@ -1,23 +1,25 @@
 import "reflect-metadata"; // DIを行うInversifyを使用するために必要
 import UserController from "@controllers/UserController";
 import UserRepositoryImpl from "@repositories/UserRepositoryImpl";
+import User from "@entities/User";
 
+// リポジトリのモック化
 jest.mock("@repositories/UserRepositoryImpl.ts");
 const UserRepositoryImplMock = UserRepositoryImpl as jest.Mock;
+const testUser: User = {
+  id: 1,
+  name: "name",
+  age: 100,
+};
+UserRepositoryImplMock.mockImplementationOnce(() => {
+  return {
+    findById: (): User => {
+      return testUser;
+    },
+  };
+});
 
 test("単一のユーザを取得できる", async () => {
-  const testUser = {
-    id: 1,
-    name: "name",
-    age: 100,
-  };
-  UserRepositoryImplMock.mockImplementationOnce(() => {
-    return {
-      findById: () => {
-        return testUser;
-      },
-    };
-  });
   const userRepositoryImpl = new UserRepositoryImpl();
   const userController = new UserController(userRepositoryImpl);
   expect(await userController.find(1)).toBe(testUser);
